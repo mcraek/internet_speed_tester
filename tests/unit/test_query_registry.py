5e46340d6a856b7f88e7e23764bc2ffaff10d4dd
Kyle McRae <kylemcrae770@gmail.com>
2020-05-24 21:59:21 -0400

    Began adding test_query_registry unit tests

--- tests/unit/test_query_registry.py
+++ tests/unit/test_query_registry.py
@@ -1,12 +1,35 @@
+# === Import required functions / libraries ===
+
+# --- Built-in ---
+
 import unittest
 from unittest.mock import patch
 
-# class TestRegQuery(unittest.TestCase):
+# --- Built for project ---
 
-#     def test_reg_connection(self):
+from internet_speed_tester.registry_functions.query_registry \
+    import connect_registry, check_root_key, check_subkey
 
-#         with patch('winreg.ConnectRegistry') as mock_reg_connection:
+# Mock working connection to registry
 
-#             mock_reg_connection.return_value = 0   
-#             query_registry.connect_registry()
 
+def test_connect_registry():
+
+    with patch('internet_speed_tester.registry_functions.query_registry.winreg'):
+
+        reg_connection = connect_registry(
+            {'log': False, 'verbose': False}, 'log')
+
+        assert reg_connection is not None
+
+
+# Mock winreg_ConnectRegistry raising an Error
+# Per documentation, it will raise on
+# OSError: https://docs.python.org/3/library/winreg.html
+
+
+def test_connect_registry_error():
+
+    with patch('internet_speed_tester.registry_functions.query_registry.winreg.ConnectRegistry', unittest.mock.MagicMock(side_effect=OSError)):
+
+        assert connect_registry({'log': False, 'verbose': False}, 'log') is None
